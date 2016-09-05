ba8c18ed483ddfff107d0522bd6edec707a52ed1
Erik De Rijcke <derijcke.erik@gmail.com>
2016-09-05 20:24:33 +0200

    remove xkb logic from wayland input device keyboard. monocle has it's own crude implementation offering the same functionality

--- src/main/java/com/sun/glass/ui/monocle/WaylandInputDeviceKeyboard.java
+++ src/main/java/com/sun/glass/ui/monocle/WaylandInputDeviceKeyboard.java
@@ -2,11 +2,12 @@ package com.sun.glass.ui.monocle;
 
 import com.google.auto.factory.AutoFactory;
 import com.google.auto.factory.Provided;
-import org.freedesktop.jaccall.Pointer;
+import com.sun.glass.events.KeyEvent;
 import org.freedesktop.wayland.client.WlKeyboardEventsV5;
 import org.freedesktop.wayland.client.WlKeyboardProxy;
 import org.freedesktop.wayland.client.WlSeatProxy;
 import org.freedesktop.wayland.client.WlSurfaceProxy;
+import org.freedesktop.wayland.shared.WlKeyboardKeyState;
 
 import javax.annotation.Nonnull;
 import java.nio.ByteBuffer;
@@ -14,33 +15,22 @@ import java.nio.ByteBuffer;
 @AutoFactory(allowSubclasses = true)
 public class WaylandInputDeviceKeyboard implements InputDevice, WlKeyboardEventsV5 {
 
-    //should be accessed by jfx thread only!
+    //should be accessed by jfx thread only! ->
+    @Nonnull
+    private final KeyInput keyInput = KeyInput.getInstance();
     @Nonnull
     private final KeyState keyState = new KeyState();
+    //<-
 
     @Nonnull
     private final WaylandPlatform waylandPlatform;
     @Nonnull
-    private final Libc            libc;
-    @Nonnull
-    private final Libxkbcommon    libxkbcommon;
-    @Nonnull
     private final WlKeyboardProxy wlKeyboardProxy;
 
-    private final long xkbContext;
-    private       long state;
-
     WaylandInputDeviceKeyboard(@Provided @Nonnull final WaylandPlatform waylandPlatform,
-                               @Provided @Nonnull final Libc libc,
-                               @Provided @Nonnull final Libxkbcommon libxkbcommon,
                                @Nonnull final WlSeatProxy wlSeatProxy) {
         this.waylandPlatform = waylandPlatform;
-        this.libc = libc;
-        this.libxkbcommon = libxkbcommon;
         this.wlKeyboardProxy = wlSeatProxy.getKeyboard(this);
-
-        //init xkbcommon. it will keep track of keys on a per keyboard base.
-        this.xkbContext = this.libxkbcommon.xkb_context_new(Libxkbcommon.XKB_CONTEXT_NO_FLAGS);
     }
 
     @Override
@@ -73,43 +63,7 @@ public class WaylandInputDeviceKeyboard implements InputDevice, WlKeyboardEvents
                        final int format,
                        final int fd,
                        final int size) {
-        //we offload keymap events to the jfx thread as it's interpretation influence subsequent key events
-        //which are also handled/converted on the jfx thread
-        this.waylandPlatform.getRunnableProcessor()
-                            .invokeLater(() -> keymapEvent(format,
-                                                           fd,
-                                                           size));
-    }
-
-    private void keymapEvent(final int format,
-                             final int fd,
-                             final int size) {
-        if (this.state != 0L) {
-            //TODO what to do with previous state?
-        }
-
-        //update keymap, used by key and modifiers events. http://xkbcommon.org/doc/current/md_doc_quick-guide.html
-        final long file = this.libc.fdopen(fd,
-                                           Pointer.nref("r").address);
-        if (file == 0L) {
-            //TODO handle failure
-        }
-
-        //get a pointer to gc enabled memory.
-        final Pointer<String> contents = Pointer.nref(new String(new char[size]));
-        this.libc.fgets(contents.address,
-                        size,
-                        file);
-
-        final long keymap = this.libxkbcommon.xkb_keymap_new_from_string(this.xkbContext,
-                                                                         contents.address,
-                                                                         format,
-                                                                         Libxkbcommon.XKB_CONTEXT_NO_FLAGS);
-        if (keymap == 0L) {
-            //TODO handle failure
-        }
-
-        this.state = this.libxkbcommon.xkb_state_new(keymap);
+        //NOOP monocle does it own kind of crude keymap thingy it seems.
     }
 
     @Override
@@ -145,11 +99,166 @@ public class WaylandInputDeviceKeyboard implements InputDevice, WlKeyboardEvents
                           final int time,
                           final int key,
                           final int state) {
-        KeyInput.getInstance()
-                .getState(this.keyState);
+        this.keyInput.getState(this.keyState);
+        final int virtualKeyCode = getVirtualKeyCode(key);
+        if (state == WlKeyboardKeyState.PRESSED.value) {
+            this.keyState.pressKey(virtualKeyCode);
+        }
+        else {
+            this.keyState.releaseKey(virtualKeyCode);
+        }
+        this.keyInput.setState(this.keyState);
+    }
 
-        //TODO convert wayland key event to jfx key event
-
+    //copy-pasta from LinuxKeyProcessor
+    private int getVirtualKeyCode(final int linuxKeyCode) {
+        if (linuxKeyCode >= LinuxInput.KEY_1 && linuxKeyCode <= LinuxInput.KEY_9) {
+            return linuxKeyCode - LinuxInput.KEY_1 + KeyEvent.VK_1;
+        }
+        else if (linuxKeyCode >= LinuxInput.KEY_NUMERIC_0 && linuxKeyCode <= LinuxInput.KEY_NUMERIC_9) {
+            return linuxKeyCode - LinuxInput.KEY_NUMERIC_0 + KeyEvent.VK_0;
+        }
+        else if (linuxKeyCode >= LinuxInput.KEY_F1 && linuxKeyCode <= LinuxInput.KEY_F10) {
+            return linuxKeyCode - LinuxInput.KEY_F1 + KeyEvent.VK_F1;
+        }
+        else if (linuxKeyCode >= LinuxInput.KEY_F11 && linuxKeyCode <= LinuxInput.KEY_F12) {
+            return linuxKeyCode - LinuxInput.KEY_F11 + KeyEvent.VK_F11;
+        }
+        else if (linuxKeyCode >= LinuxInput.KEY_F13 && linuxKeyCode <= LinuxInput.KEY_F24) {
+            return linuxKeyCode - LinuxInput.KEY_F13 + KeyEvent.VK_F13;
+        }
+        else {
+            switch (linuxKeyCode) {
+                case LinuxInput.KEY_0:
+                    return KeyEvent.VK_0;
+                case LinuxInput.KEY_A:
+                    return KeyEvent.VK_A;
+                case LinuxInput.KEY_B:
+                    return KeyEvent.VK_B;
+                case LinuxInput.KEY_C:
+                    return KeyEvent.VK_C;
+                case LinuxInput.KEY_D:
+                    return KeyEvent.VK_D;
+                case LinuxInput.KEY_E:
+                    return KeyEvent.VK_E;
+                case LinuxInput.KEY_F:
+                    return KeyEvent.VK_F;
+                case LinuxInput.KEY_G:
+                    return KeyEvent.VK_G;
+                case LinuxInput.KEY_H:
+                    return KeyEvent.VK_H;
+                case LinuxInput.KEY_I:
+                    return KeyEvent.VK_I;
+                case LinuxInput.KEY_J:
+                    return KeyEvent.VK_J;
+                case LinuxInput.KEY_K:
+                    return KeyEvent.VK_K;
+                case LinuxInput.KEY_L:
+                    return KeyEvent.VK_L;
+                case LinuxInput.KEY_M:
+                    return KeyEvent.VK_M;
+                case LinuxInput.KEY_N:
+                    return KeyEvent.VK_N;
+                case LinuxInput.KEY_O:
+                    return KeyEvent.VK_O;
+                case LinuxInput.KEY_P:
+                    return KeyEvent.VK_P;
+                case LinuxInput.KEY_Q:
+                    return KeyEvent.VK_Q;
+                case LinuxInput.KEY_R:
+                    return KeyEvent.VK_R;
+                case LinuxInput.KEY_S:
+                    return KeyEvent.VK_S;
+                case LinuxInput.KEY_T:
+                    return KeyEvent.VK_T;
+                case LinuxInput.KEY_U:
+                    return KeyEvent.VK_U;
+                case LinuxInput.KEY_V:
+                    return KeyEvent.VK_V;
+                case LinuxInput.KEY_W:
+                    return KeyEvent.VK_W;
+                case LinuxInput.KEY_X:
+                    return KeyEvent.VK_X;
+                case LinuxInput.KEY_Y:
+                    return KeyEvent.VK_Y;
+                case LinuxInput.KEY_Z:
+                    return KeyEvent.VK_Z;
+                case LinuxInput.KEY_LEFTCTRL:
+                case LinuxInput.KEY_RIGHTCTRL:
+                    return KeyEvent.VK_CONTROL;
+                case LinuxInput.KEY_LEFTSHIFT:
+                case LinuxInput.KEY_RIGHTSHIFT:
+                    return KeyEvent.VK_SHIFT;
+                case LinuxInput.KEY_CAPSLOCK:
+                    return KeyEvent.VK_CAPS_LOCK;
+                case LinuxInput.KEY_TAB:
+                    return KeyEvent.VK_TAB;
+                case LinuxInput.KEY_GRAVE:
+                    return KeyEvent.VK_BACK_QUOTE;
+                case LinuxInput.KEY_MINUS:
+                    return KeyEvent.VK_MINUS;
+                case LinuxInput.KEY_EQUAL:
+                    return KeyEvent.VK_EQUALS;
+                case LinuxInput.KEY_BACKSPACE:
+                    return KeyEvent.VK_BACKSPACE;
+                case LinuxInput.KEY_LEFTBRACE:
+                    return KeyEvent.VK_BRACELEFT;
+                case LinuxInput.KEY_RIGHTBRACE:
+                    return KeyEvent.VK_BRACERIGHT;
+                case LinuxInput.KEY_BACKSLASH:
+                    return KeyEvent.VK_BACK_SLASH;
+                case LinuxInput.KEY_SEMICOLON:
+                    return KeyEvent.VK_SEMICOLON;
+                case LinuxInput.KEY_APOSTROPHE:
+                    return KeyEvent.VK_QUOTE;
+                case LinuxInput.KEY_COMMA:
+                    return KeyEvent.VK_COMMA;
+                case LinuxInput.KEY_DOT:
+                    return KeyEvent.VK_PERIOD;
+                case LinuxInput.KEY_SLASH:
+                    return KeyEvent.VK_SLASH;
+                case LinuxInput.KEY_LEFTALT:
+                case LinuxInput.KEY_RIGHTALT:
+                    return KeyEvent.VK_ALT;
+                case LinuxInput.KEY_LEFTMETA:
+                case LinuxInput.KEY_RIGHTMETA:
+                    return KeyEvent.VK_COMMAND;
+                case LinuxInput.KEY_SPACE:
+                    return KeyEvent.VK_SPACE;
+                case LinuxInput.KEY_MENU:
+                    return KeyEvent.VK_CONTEXT_MENU;
+                case LinuxInput.KEY_ENTER:
+                    return KeyEvent.VK_ENTER;
+                case LinuxInput.KEY_LEFT:
+                    return KeyEvent.VK_LEFT;
+                case LinuxInput.KEY_RIGHT:
+                    return KeyEvent.VK_RIGHT;
+                case LinuxInput.KEY_UP:
+                    return KeyEvent.VK_UP;
+                case LinuxInput.KEY_DOWN:
+                    return KeyEvent.VK_DOWN;
+                case LinuxInput.KEY_HOME:
+                    return KeyEvent.VK_HOME;
+                case LinuxInput.KEY_DELETE:
+                    return KeyEvent.VK_DELETE;
+                case LinuxInput.KEY_INSERT:
+                    return KeyEvent.VK_INSERT;
+                case LinuxInput.KEY_END:
+                    return KeyEvent.VK_END;
+                case LinuxInput.KEY_PAGEDOWN:
+                    return KeyEvent.VK_PAGE_DOWN;
+                case LinuxInput.KEY_PAGEUP:
+                    return KeyEvent.VK_PAGE_UP;
+                case LinuxInput.KEY_NUMLOCK:
+                    return KeyEvent.VK_NUM_LOCK;
+                case LinuxInput.KEY_ESC:
+                    return KeyEvent.VK_ESCAPE;
+                case LinuxInput.KEY_NUMERIC_STAR:
+                    return KeyEvent.VK_MULTIPLY;
+                default:
+                    return KeyEvent.VK_UNDEFINED;
+            }
+        }
     }
 
     @Override
@@ -159,31 +268,14 @@ public class WaylandInputDeviceKeyboard implements InputDevice, WlKeyboardEvents
                           final int modsLatched,
                           final int modsLocked,
                           final int group) {
-        this.waylandPlatform.getRunnableProcessor()
-                            .invokeLater(() -> modifiersEvent(serial,
-                                                              modsDepressed,
-                                                              modsLatched,
-                                                              modsLocked,
-                                                              group));
-    }
-
-    private void modifiersEvent(final int serial,
-                                final int modsDepressed,
-                                final int modsLatched,
-                                final int modsLocked,
-                                final int group) {
-        KeyInput.getInstance()
-                .getState(this.keyState);
-
-        //TODO convert wayland modifiers event to jfx key event
-
+        //NOOP. monocle has a fixed hard coded list of what is considered a modifier and what isn't.
     }
 
     @Override
     public void repeatInfo(final WlKeyboardProxy emitter,
                            final int rate,
                            final int delay) {
-        //TODO? does jfx care?
+        //NOOP.
     }
 
     @Nonnull
